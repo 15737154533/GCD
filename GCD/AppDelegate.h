10e67c19b153efa6a8301063045599679c74cf12
mac <mac@macdeiMac.local>
2017-03-25 15:18:04 +0800

    Initial Commit

---
+++ GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by mac on 2017/3/25.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by mac on 2017/3/25.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
